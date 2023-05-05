629689fbf9c763f78a8fb76ac7e38f4967b38bd1
Bivas Das <49869550+dasbivas@users.noreply.github.com>
2023-05-05 19:38:03 +0530

    Update minikube.sh

--- minikube.sh
+++ minikube.sh
@@ -10,8 +10,11 @@ chmod +x minikube
 sudo mv minikube /usr/local/bin/
 minikube version
 sudo apt install conntrack
+sysctl net.ipv6.conf.all.disable_ipv6=0
 curl -LO https://get.helm.sh/helm-v3.10.0-linux-amd64.tar.gz
 tar -xvzf helm-v3.10.0-linux-amd64.tar.gz
 mv linux-amd64/helm /usr/local/bin/helm
 
-sudo minikube start --vm-driver=none
+sudo minikube start --vm-driver=none 
+sudo minikube start --vm-driver=none --apiserver-ips=<IP1>,<IP2>
+
