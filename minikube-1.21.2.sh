ef1ffb7b10f241123d04fb18c19eb6eee72c84ee
Bivas Das <49869550+dasbivas@users.noreply.github.com>
2023-06-27 21:58:23 +0530

    Update minikube-1.21.2.sh

--- minikube-1.21.2.sh
+++ minikube-1.21.2.sh
@@ -2,7 +2,7 @@ curl -LO https://dl.k8s.io/release/v1.22.5/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 sudo apt-get update && \
-sudo apt-get install docker.io -y
+sudo apt-get install docker.io jq -y
 sudo systemctl start docker 
 curl -LO https://github.com/kubernetes/minikube/releases/download/v1.22.0/minikube-linux-amd64
 mv minikube-linux-amd64 minikube
@@ -15,8 +15,8 @@ curl -LO https://get.helm.sh/helm-v3.10.0-linux-amd64.tar.gz
 tar -xvzf helm-v3.10.0-linux-amd64.tar.gz
 mv linux-amd64/helm /usr/local/bin/helm
 
-sudo minikube start --vm-driver=none 
 #sudo minikube start --vm-driver=none --apiserver-ips=<IP1>,<IP2>
+sudo minikube start --vm-driver=none 
 
 #sudo minikube start \
 #       --vm-driver=none \
